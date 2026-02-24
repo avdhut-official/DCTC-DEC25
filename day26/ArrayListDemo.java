cf6309658378ea71995c067dc96c18a3d4abbffe
avdhut-official <avdhut.bhiseofficial@gmail.com>
2026-02-25 00:17:37 +0530

    Uploading till Collection-Array List

--- day26/ArrayListDemo.java
+++ day26/ArrayListDemo.java
@@ -6,7 +6,7 @@ public class ArrayListDemo {
 	
 	public static void main(String[] args) {
 		
-		ArrayList<Integer> list=new ArrayList();
+		ArrayList<Integer> list=new ArrayList();		//only int objects can stored
 		list.add(15);
 		list.add(25);
 		list.add(35);
@@ -14,12 +14,14 @@ public class ArrayListDemo {
 		
 		System.out.println(list);
 		
-		list.remove(0);
+		list.remove(0);			//this will remove element at 0th location
 		System.out.println(list);
 		
-		list.add(1, 58);
+		list.add(1, 58);		//method overloading - this method will insert object at 1st location 
 		System.out.println(list);
 		
+		
+		//used for displaying the array list object using for each 
 		for(Integer x : list)
 		{
 			System.out.println(x);
